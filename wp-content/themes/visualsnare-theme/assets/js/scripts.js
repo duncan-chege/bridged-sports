53b117795f7f2c61cce03595540b495f48b2f4f3
viisualworks <dshege4@gmail.com>
2024-08-06 12:40:51 +0300

    change navbar on scroll & in brief section on homepage

--- wp-content/themes/visualsnare-theme/assets/js/scripts.js
+++ wp-content/themes/visualsnare-theme/assets/js/scripts.js
@@ -11,10 +11,31 @@ document.addEventListener('DOMContentLoaded', function () {
     });
     hero_splide.mount();
 
+    var home_about = new Splide('.about.splide', {
+        type   : 'fade', // Use fade effect for smooth transitions
+        rewind: true,
+        pagination: false,
+        arrows: false,
+        autoplay: true,
+        interval: 3000,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+    });
+    home_about.mount();
+
     var testimonial_splide = new Splide('.testimonials .splide', {
        type: "loop",
        pagination: false
     });
     testimonial_splide.mount();
+
+    var navbar = document.querySelector(".navbar");
+    window.addEventListener("scroll", function() {
+        if (window.scrollY > 50) {
+            navbar.classList.add("navbar-scrolled");
+        } else {
+            navbar.classList.remove("navbar-scrolled");
+        }
+    });
     
 });
