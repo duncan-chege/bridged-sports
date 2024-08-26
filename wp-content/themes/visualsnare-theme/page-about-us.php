c2c1e727070061b93629bda7edeb13a9ea55efec
viisualworks <dshege4@gmail.com>
2024-08-26 12:57:09 +0300

    edit the about page content

--- wp-content/themes/visualsnare-theme/page-about-us.php
+++ wp-content/themes/visualsnare-theme/page-about-us.php
@@ -7,14 +7,14 @@ get_header(); ?>
 <div class="home splide" role="group" aria-label="Homepage Splide">
     <div class="splide__track">
         <ul class="splide__list">
-            <li class="splide__slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/sport-photographer.png"
-                    alt="" /></li>
             <li class="splide__slide"><img
-                    src="<?php echo get_template_directory_uri(); ?>/assets/img/party-goers.png" alt="" /></li>
+                    src="<?php echo get_template_directory_uri(); ?>/assets/img/sport-photographer.png" alt="" /></li>
+            <li class="splide__slide"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/party-goers.png"
+                    alt="" /></li>
         </ul>
     </div>
     <div class="header-title">
-    <h1><?php echo get_the_title(); ?></h1>
+        <h1><?php echo get_the_title(); ?></h1>
     </div>
 </div>
 
@@ -23,27 +23,11 @@ get_header(); ?>
         <div>
             <h2>Our Mission</h2>
             <p>To show that Sports and Entertainment are viable industries with vast career options for young aspiring
-                individuals.</p>
-            <p>We do this by creating better access to industry information and resources and sharing the experiences of
-                those in the industries.</p>
+                individuals by creating better access to information, opportunities, and resources</p>
         </div>
         <div class="bloc-img"></div>
     </div>
     <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mission-wave.svg" alt="" class="mission-wave" />
-</div>
-
-<div class="vision">
-    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/vision-wave.svg" alt="" class="vision-wave" />
-    <div class="bloc">
-        <div class="bloc-img"></div>
-        <div>
-            <h2>Why We Do This</h2>
-            <p>We do this to enhance the visibility of diverse professionals in the Sports and Entertainment industries
-                and to provide better access to information</p>
-            <p>This will improve the perception of these fields as viable career options for young individuals.</p>
-        </div>
-    </div>
-
 </div>
 
 <div class="founder">
@@ -60,7 +44,8 @@ get_header(); ?>
         <div class="row">
             <div class="col-md-4 text-center my-3">
                 <div class="card">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ella-ireri-headshot.jpg" alt="" class="card-img-top" />
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/ella-ireri-headshot.jpg" alt=""
+                        class="card-img-top" />
                     <div class="card-body">
                         <h5 class="card-title"> Ella Ireri </h5>
                         <p class="card-text"> Founder </p>
@@ -68,20 +53,12 @@ get_header(); ?>
                 </div>
             </div>
             <div class="col-md-4 text-center my-3">
-            <div class="card">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mweni-photo.png" alt="" class="card-img-top" />
+                <div class="card">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/mweni-photo.png" alt=""
+                        class="card-img-top" />
                     <div class="card-body">
                         <h5 class="card-title"> Mweni Chola </h5>
                         <p class="card-text"> Social Media & Content Writer </p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 text-center my-3">
-            <div class="card">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img//ella-ireri-headshot.jpg" alt="" class="card-img-top" />
-                    <div class="card-body">
-                        <h5 class="card-title"> Ella Ireri </h5>
-                        <p class="card-text"> Founder </p>
                     </div>
                 </div>
             </div>
